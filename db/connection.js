12a66ba4ef71775ef59d9a60943a01f70ddf24ae
toblan9991 <oluwatobiodulana@gmail.com>
2024-02-24 18:36:42 -0800

    first commit

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+let mongoDB = ;
+
+////EXAMPLE////
+// Make sure to create .env file and use an environment variable
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = mongoose.connection;
